f8b7723fb306ae3f10ec8f55ab72e3a7ec1feef2
MaratWorker <maratn566ek@gmail.com>
2020-10-26 08:45:29 +0300

    finish easy CreatingHash example

--- src/main.rs
+++ src/main.rs
@@ -3,59 +3,78 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-#[link(name="ssp", kind ="dylib")]
-#[link(name="capi10", kind ="dylib")]
-#[link(name="capi20", kind ="dylib")]
-#[link(name="rdrsup", kind ="dylib")]
-#[link(name="pthread", kind ="dylib")]
-
+#[link(name = "ssp", kind = "dylib")]
+#[link(name = "capi10", kind = "dylib")]
+#[link(name = "capi20", kind = "dylib")]
+#[link(name = "rdrsup", kind = "dylib")]
+#[link(name = "pthread", kind = "dylib")]
 use std::env;
 use std::fs;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
 
-const BUFSIZE : usize = 1024;
-const GR3411LEN : usize = 64;
-fn main() ->  Result<(), i32> {
-    let r : i32 = 32;
+const BUFSIZE: usize = 1024;
+const GR3411LEN: DWORD = 64;
+fn main() -> Result<(), i32> {
+    let r: i32 = 32;
 
     let bIsReadingFailed: BOOL = FALSE as BOOL;
-    let mut hProv: HCRYPTPROV  = 0;
-    let mut hHash: HCRYPTHASH  = 0;
+    let mut hProv: HCRYPTPROV = 0;
+    let mut hHash: HCRYPTHASH = 0;
     let hFile: FILE;
     let rgbFile: [BYTE; BUFSIZE];
-    let cbRead: DWORD  = 0;
-    let rgbHash: [BYTE; GR3411LEN];
-    let cbHash: DWORD  = 0;
-    // let rgbDigits: [CHAR] = "0123456789abcdef";
+    let cbRead: DWORD = 0;
+    // let rgbHash: [BYTE; GR3411LEN];
+    let mut cbHash: DWORD = 0;
+    const rgbDigits:&str = "0123456789abcdef";
     let i: DWORD;
 
     let argc = env::args().count();
     let argv: Vec<String> = env::args().collect();
-    println!("Argv {}, {}",argv[0], argv[0]);
-    if(argc != 2 || argv[1] == "")
-    {
-       HandleError("The file name is absent.\n");
-       return Err(1);
+    println!("Argv {}, {}", argv[0], argv[0]);
+    if (argc != 2 || argv[1] == "") {
+        HandleError("The file name is absent.\n");
+        return Err(1);
     }
 
-    println!("Reading args success: count {} argv: {:?}",argc, argv);
+    println!("Reading args success: count {} argv: {:?}", argc, argv);
 
     let filename = argv[1].clone();
     println!("In file {}", filename);
 
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-        
+    let mut file = File::open(&filename).unwrap();
+    file.sync_all();
+    let mut buffer: [BYTE;1024] = [0; 1024];
+    let mut n = file.read(&mut buffer);
+    // println!("Data buffer: {:?} size {:?}", buffer, n);
+    // for elem in buffer.iter() {
+        // print!("{} ", elem);
+    // }
+    println!("End print buf");
+    // n = file.read(&mut buffer);
+    // println!("Data buffer: {:?} size {:?}", buffer, n);
+    // while (n.unwrap() > 0) {
+        // n = file.read(&mut buffer);
+        // println!("Data buffer: {:?} size {:?}", buffer, n);
+    // }
+
+    // let data = fs::read(&filename);
+    // println!("Data : {:?}", data);
+
+    let contents = fs::read_to_string(&filename).expect("Something went wrong reading the file");
     let hprov = &mut hProv as *mut HCRYPTPROV;
     unsafe {
         // todo this function only for the CryptAcquireContextW for UNICODE
-        let status = !CryptAcquireContextW(
-        	hprov,
-        	0 as LPCWSTR,//NULL,
-        	0 as LPCWSTR,//NULL,
-        	PROV_GOST_2012_256,
-        	CRYPT_VERIFYCONTEXT);
-        println!("Status crypto acquire context:\n{}", status);
-        if (status < 0) {
+        let status = CryptAcquireContextW(
+            hprov,
+            0 as LPCWSTR, //NULL,
+            0 as LPCWSTR, //NULL,
+            PROV_GOST_2012_256,
+            CRYPT_VERIFYCONTEXT,
+        );
+        println!("Status crypto acquire context:{} hprov {}", status, *hprov);
+        if (status == 0) {
             HandleError("Status crypto acquire context");
         }
     }
@@ -63,27 +82,104 @@ fn main() ->  Result<(), i32> {
     // println!("With text:\n{}", contents);
     let hash = &mut hHash as *mut HCRYPTHASH;
     unsafe {
-        let status = !CryptCreateHash(hProv, CALG_GR3411_2012_256, 0, 0, hash);
-        println!("Status crypto create hash:\n{}", status);
-        if (status < 0) {
+        let status = CryptCreateHash(hProv, CALG_GR3411_2012_256, 0, 0, hash);
+        println!("Status crypto create hash:{}", status);
+        if (status == 0) {
+            CryptReleaseContext(hProv, 0);
             HandleError("Status crypto acquire context");
         }
     }
-    // if()
-    // {
-	// CryptReleaseContext(hProv, 0);
-	// HandleError("CryptCreateHash failed"); 
+
+    let mut i = 0;
+    let count = n.unwrap();
+    // loop {
+        let d = &buffer as *const BYTE;
+        unsafe {
+            println!("Count {}", count);
+            let status = CryptHashData(hHash, d, count as DWORD, 0);
+            println!("Status CryptHashData:{}", status);
+            if (status == 0) {
+                CryptReleaseContext(hProv, 0);
+                CryptDestroyHash(hHash);
+                HandleError("CryptHashData failed");
+            }
+        }
+
+    //     i += 1;
+    //     if (i == 15) {
+    //         println!("End loop");
+    //         break;
+    //     }
     // }
+
+    cbHash = GR3411LEN;
+    let mut pbData:[BYTE; 64]  = [0; 64];
+    println!("pbData: {:?}", pbData[0]);
+    let rgbHash = pbData.as_mut_ptr() as *mut BYTE;
+    println!("Arrray size: {}", cbHash);
+    for x in pbData.iter() {
+        print!("{} ", x);
+    }
+    unsafe {
+
+    // pub fn CryptGetHashParam(
+    //     hHash: HCRYPTHASH,
+    //     dwParam: DWORD,
+    //     pbData: *mut BYTE,
+    //     pdwDataLen: *mut DWORD,
+    //     dwFlags: DWORD,
+    // ) -> BOOL;
+    
+        let mut countHash = &mut cbHash as *mut DWORD; 
+        let status = CryptGetHashParam(hHash, HP_HASHVAL, rgbHash, countHash, 0);
+            if (status == 0) {
+                CryptReleaseContext(hProv, 0);
+                CryptDestroyHash(hHash);
+                HandleError("CryptGetHashParam failed");
+            }
+    }
+
+    println!("GR3411 hash of file {} is: ", argv[1]);
+    println!("Arrray size: {}", cbHash);
+    // println!("pbData: {:?}", pbData[0]);
+    // let vDig = unsafe {rgbDigits.as_bytes();};
+    let mut arrChar: Vec<char> = Vec::new();
+    // char c = '2';
+    for mut elem in rgbDigits.chars() {
+        arrChar.push(elem); 
+        print!("{}", elem); 
+    }
+    println!();
+    // let vDig = rgbDigits.to_string().as_bytes();
+    // vDig[0];
+    for x in pbData.iter() {
+        let it1 : usize = (x >> 4) as usize;
+        let it2 : usize= (x & 0xf) as usize;
+        print!("{}{} ", arrChar[it1], arrChar[it2]);
+        // vDig[0].to_ascii_uppercase();
+        // println!("iterators {:?} {:?}", vDig[it1].to_ascii_uppercase(), vDig[it2]);
+
+
+        // print!("dig: {} {}", dig[it], dig[1]);
+
+    }
+    println!("Finish");
+    // {
+	//     printf("%c%c", rgbDigits[rgbHash[i] >> 4], rgbDigits[rgbHash[i] & 0xf]);
+    // }
+    // printf("\n");
+
     Ok(())
 }
 
-fn HandleError(s: &str)
-{
+fn HandleError(s: &str) {
     let mut err: u32;
-    unsafe {err = GetLastError();};
+    unsafe {
+        err = GetLastError();
+    };
     println!("Error number     : {}\n", err);
     println!("Error description: {}\n", s);
-    // if(!err) 
+    // if(!err)
     //     err = 1;
     // exit(err);
 }
